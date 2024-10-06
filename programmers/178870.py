82fa0d1cf4af5cab7634a1647671c86e2ef23c86
권길남 <nimpa3201@naver.com>
2024-10-06 19:58:51 +0900

    연속된 부분 수열의 합

--- programmers/178870.py
+++ programmers/178870.py
@@ -1,41 +1,38 @@
 def solution(sequence, k):
-    L =0
-    R =0
+    L = 0
+    R = 0
     n = len(sequence)
+    curr_sum = 0
     arr = []
-    while L <=R < len(sequence):
-        sum_val =0
-        for i in range(L,R+1):
-            sum_val +=sequence[i]
-        
-        if sum_val < k :
-            R+=1
-        if sum_val > k :
-            L+=1
-        if sum_val == k:
-            arr.append((L,R))
-            R+=1
 
-    curr_L =0
-    curr_R =0
-    for L,R in arr:
-       if n > R-L:
-           n = R-L
-           curr_R = R
-           curr_L = L
-    print([curr_L,curr_R])
-    
+    while R < n:
+        curr_sum += sequence[R]
+
+        while curr_sum > k and L <= R:
+            curr_sum -= sequence[L]
+            L += 1
+
+        if curr_sum == k:
+            arr.append((L, R))
+
+        R += 1
+
+    min_len = n + 1
+    curr_L = 0
+    curr_R = 0
+    for L, R in arr:
+        if R - L < min_len:
+            min_len = R - L
+            curr_L = L
+            curr_R = R
+
+    return [curr_L, curr_R]
+
+
             
             
-            
-            
-              
-            
-    
-    
 solution([1, 2, 3, 4, 5] ,7) #[2, 3]
-
-
 solution([1, 1, 1, 2, 3, 4, 5],5)	#[6, 6]
-
 solution([2, 2, 2, 2, 2],6) #	[0, 2]
+
+#https://school.programmers.co.kr/learn/courses/30/lessons/178870 연속된 부분 수열의 합 level2
