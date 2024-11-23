146371d152956d561353481ffb9fdbedbf2bd1b6
권길남 <nimpa3201@naver.com>
2024-11-23 14:17:43 +0900

    전쟁 - 땅따먹기

--- Baekjoon/1270.py
+++ Baekjoon/1270.py
@@ -3,20 +3,18 @@ for _ in range(n):
     d= dict()
     war = list(map(int,input().split()))
     army = war[0]
-    winner =[]
     for w in war[1:]:
         if w in d :
             d[w] +=1
         else:
             d[w] =1
-    
+    flag = False
     for i in d :
-        if d[i] > army/2:
-            winner.append(i)
-    
-    if len(winner) ==0:
+        if d[i] > army//2:
+            print(i)
+            flag= True
+    if not flag:
         print("SYJKGW")
-    else:
-        print(*winner)      
+       
 
 #https://www.acmicpc.net/problem/1270 전쟁 - 땅따먹기      
